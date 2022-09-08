7c2d2d20883fb528f9241a6a1d733f0b9e2337d4
PakapolTaubol <pakapol.taubol@mail.kmutt.ac.th>
2022-09-09 01:16:33 +0700

    getMaxLength062

--- getMaxLengthString.js
+++ getMaxLengthString.js
@@ -2,5 +2,19 @@ const { template } = require('@babel/core')
 
 function getMaxLengthString(arrayOfString) {
   //code here
+  if (arrayOfString === undefined || arrayOfString === null) { 
+    return undefined 
+  }
+  else {
+    let ans = [arrayOfString[0]]
+    for (let i = 1 ; i < arrayOfString.length; i++) {
+      if (arrayOfString[i].length > ans[0].length) {
+        ans = [arrayOfString[i]] 
+      }
+      else if(arrayOfString[i].length === ans[0].length) {
+        ans.push(arrayOfString[i]) // ถ้าเจอค่าเท่ากันตัวนี้จะทำให้ดึงมาอยู่ด้วย
+      }
+    } return ans 
+  }
 }
 module.exports = getMaxLengthString
